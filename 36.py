e2ebb3c316a0a9e2e29d7beb4ec04275369bef01
yeyae <whyjinny@naver.com>
2022-08-09 11:53:52 +0900

    python

---
+++ 36.py
@@ -0,0 +1,16 @@
+#결괏값이 없는 함수
+from re import A
+
+
+def add(a,b):
+    print("%d, %d의 합은 %d입니다." % (a, b, a+b))
+
+add(3,4)
+
+#함수 이름(매개변수 1, 매개변수 2, ...)
+
+a = add(3,4)
+
+a = add(3,4)
+print(a)
+#None이란 거짓을 나타내는 자료형 
